be6d5e78429b7be1a27da518a39a5b42201c440d
Shashank-byts <shashankshekhar172003@gmail.com>
2025-09-02 20:47:53 +0000

    config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" // treate all JS code as newer version
+// alert(3+3) we are using node js not browser 
+let name ="shashank"
+let age=18
+let isLogdedIn =true
+/*
+types of dataTypes 
+number 
+bigint
+string 
+boolean
+null =>stand alone value 
+undefined
+symbol=>unique 
+object
+*/
+// now to know the type of any variable we use 
+console.log(typeof age)
+console.log(typeof "shashank");
+// we can use console.log(typeof ) to know the type of data 
+console.log(typeof undefined);
+console.log(typeof null);
+// point to be noted here that null is a object and undefined is a data type 
+
+
